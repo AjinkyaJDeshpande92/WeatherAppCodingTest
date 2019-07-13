dbd99538c636612a59dd4547c04683291b52a57d
AjinkyaJD <Ajinkya@1992>
2019-07-13 11:01:12 +0800

    [AJ] CHANGED - Api Call to Weather Details.

--- app/src/main/java/com/ajinkyad/weatherApp/repository/WeatherRepository.java
+++ app/src/main/java/com/ajinkyad/weatherApp/repository/WeatherRepository.java
@@ -3,6 +3,7 @@ package com.ajinkyad.weatherApp.repository;
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 
+import com.ajinkyad.weatherApp.BuildConfig;
 import com.ajinkyad.weatherApp.repository.model.WeatherResponse;
 
 import javax.inject.Inject;
@@ -22,30 +23,21 @@ public class WeatherRepository {
         this.weatherAPI = weatherAPI;
     }
 
-    public LiveData<WeatherResponse> getOrders() {
+    public LiveData<WeatherResponse> getWeatherDetails(String cityName) {
         final MutableLiveData<WeatherResponse> data = new MutableLiveData<>();
 
-        weatherAPI.getOrders().enqueue(new Callback<WeatherResponse>() {
+        weatherAPI.getWeatherDetails(cityName, BuildConfig.WEATHER_API_KEY).enqueue(new Callback<WeatherResponse>() {
             @Override
             public void onResponse(Call<WeatherResponse> call, Response<WeatherResponse> response) {
-//                if (response.code() == 200) {
-//                    data.setValue(response.body());
-//                } else if (response.code() == 503) {
-//                    DahmakanResponse d = new DahmakanResponse();
-//                    d.setError("Service unavilable, please try later");
-//                    data.setValue(d);
-//                } else {
-//                    DahmakanResponse d = new DahmakanResponse();
-//                    d.setError("Some error occurred, please try later");
-//                    data.setValue(d);
-//                }
+
+                if (response.isSuccessful()) {
+                    response.body();
+                }
             }
 
             @Override
             public void onFailure(Call<WeatherResponse> call, Throwable t) {
-//                DahmakanResponse d = new DahmakanResponse();
-//                d.setError("Some error occurred, please try later");
-//                data.setValue(d);
+
             }
         });
         return data;
